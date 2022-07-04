453f10706598a9fc005023a886a0b0451915e03d
Chidi Jossy <cjossywike546@gmail.com>
2022-07-04 14:04:08 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	puts("with a proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
