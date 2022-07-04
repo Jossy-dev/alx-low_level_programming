5fb75d810d40f102afa1a6b624aec356ee89698c
Chidi Jossy <cjossywike546@gmail.com>
2022-07-04 16:47:13 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all the numbers in base 16, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar(10);
+
+	return (0);
+}
